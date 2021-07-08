34caa596aa04d4d0e6133ac52b16e8c44a0c84f5
chaitanya-ajency <chaitanya@ajency.in>
2021-07-09 01:27:14 +0530

    extra form issues

--- signup/js/signup.js
+++ signup/js/signup.js
@@ -474,3 +474,31 @@ $('#acceptQBtn1').on('click', function (e) {
 
         }
     });
+    $(".toggle-password").click(function() {
+
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+          input.attr("type", "text");
+        } else {
+          input.attr("type", "password");
+        }
+      });
+
+
+
+
+
+
+
+
+      $(".password-eye").click(function() {
+
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+          input.attr("type", "text");
+        } else {
+          input.attr("type", "password");
+        }
+      });
