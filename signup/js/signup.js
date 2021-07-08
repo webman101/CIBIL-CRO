6c87430b619599ac6d8f3d222dfbbf7e2020ff94
chaitanya-ajency <chaitanya@ajency.in>
2021-07-08 15:43:39 +0530

    loan number

--- signup/js/signup.js
+++ signup/js/signup.js
@@ -404,3 +404,34 @@ $('#applyCouponCode').click(function(){
         $('.price-block-descount').addClass('hide');
    }
 });
+$('#cardNumber').on('keyup', function(e) {
+    var val = $(this).val();
+    var newval = '';
+    val = val.replace(/\s/g, '');
+    for (var i = 0; i < val.length; i++) {
+        if (i % 4 == 0 && i > 0) newval = newval.concat(' ');
+        newval = newval.concat(val[i]);
+    }
+    $(this).val(newval);
+});
+$('#cardNumber2').on('keyup', function(e) {
+    var val = $(this).val();
+    var newval = '';
+    val = val.replace(/\s/g, '');
+    for (var i = 0; i < val.length; i++) {
+        if (i % 4 == 0 && i > 0) newval = newval.concat(' ');
+        newval = newval.concat(val[i]);
+    }
+    $(this).val(newval);
+});
+$('#cardNumber3').on('keyup', function(e) {
+    var val = $(this).val();
+    var newval = '';
+    val = val.replace(/\s/g, '');
+    for (var i = 0; i < val.length; i++) {
+        if (i % 4 == 0 && i > 0) newval = newval.concat(' ');
+        newval = newval.concat(val[i]);
+    }
+    $(this).val(newval);
+
+});
