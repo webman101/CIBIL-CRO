a3c4abd38956e208680a85bfb10efee3aab37b43
chaitanya-ajency <chaitanya@ajency.in>
2021-07-09 12:57:19 +0530

    loan input

--- signup/js/signup.js
+++ signup/js/signup.js
@@ -404,37 +404,7 @@ $('#applyCouponCode').click(function(){
         $('.price-block-descount').addClass('hide');
    }
 });
-$('#cardNumber').on('keyup', function(e) {
-    var val = $(this).val();
-    var newval = '';
-    val = val.replace(/\s/g, '');
-    for (var i = 0; i < val.length; i++) {
-        if (i % 4 == 0 && i > 0) newval = newval.concat(' ');
-        newval = newval.concat(val[i]);
-    }
-    $(this).val(newval);
-});
-$('#cardNumber2').on('keyup', function(e) {
-    var val = $(this).val();
-    var newval = '';
-    val = val.replace(/\s/g, '');
-    for (var i = 0; i < val.length; i++) {
-        if (i % 4 == 0 && i > 0) newval = newval.concat(' ');
-        newval = newval.concat(val[i]);
-    }
-    $(this).val(newval);
-});
-$('#cardNumber3').on('keyup', function(e) {
-    var val = $(this).val();
-    var newval = '';
-    val = val.replace(/\s/g, '');
-    for (var i = 0; i < val.length; i++) {
-        if (i % 4 == 0 && i > 0) newval = newval.concat(' ');
-        newval = newval.concat(val[i]);
-    }
-    $(this).val(newval);
 
-});
 $('#acceptQBtn1').on('click', function (e) {
     var validate = true;
     var url = '/CIBIL-CRO/signup/pair-device.html';
