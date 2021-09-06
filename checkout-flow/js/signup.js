a0a3e250a4b08f6885f98daa8839a01d194e5c14
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-09-06 10:43:19 +0530

    159

--- checkout-flow/js/signup.js
+++ checkout-flow/js/signup.js
@@ -1,19 +1,10 @@
-//////////////
-let digitValidate = function(ele) {
-    ele.value = ele.value.replace(/[^0-9]/g, '');
-}
-var elems = document.querySelectorAll(".widget.hover");
-let tabChange = function(val) {
-    let ele = document.querySelectorAll('input');
-    if (ele[val - 1].value != '') {
-        ele[val].focus();
-        ele.classList.add("mystyle");
+$(".otp").keyup(function () {
+    if (this.value.length == this.maxLength) {
+      $(this).next('.otp').focus();
+      $(this).removeClass("input-focus").next('.otp').addClass('input-focus');
 
-    } else if (ele[val - 1].value == '') {
-        ele[val - 2].focus()
     }
-}
-
+});
 
 $('.otp-6').keyup(function(e) {
     if ($(this).val() != '') {
