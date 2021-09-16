bfb6a68142cfb9612703091be7c3abd8b4b4e944
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-09-16 15:44:20 +0530

    new changes

--- beworthy/js/custom.js
+++ beworthy/js/custom.js
@@ -1,18 +1,39 @@
-$(function () {
+$(function() {
 
     var activeIndex = $('.active-tab').index(),
         $contentlis = $('.tabs-content li'),
         $tabslis = $('.tabs li');
-    
+
     // Show content of active tab on loads
     $contentlis.eq(activeIndex).show();
-  
-    $('.tabs').on('click', 'li', function (e) {
-      var $current = $(e.currentTarget),
-          index = $current.index();
-      
-      $tabslis.removeClass('active-tab');
-      $current.addClass('active-tab');
-      $contentlis.hide().eq(index).show();
-       });
-  });
+
+    $('.tabs').on('click', 'li', function(e) {
+        var $current = $(e.currentTarget),
+            index = $current.index();
+
+        $tabslis.removeClass('active-tab');
+        $current.addClass('active-tab');
+        $contentlis.hide().eq(index).show();
+    });
+});
+
+
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+    accordion.onclick = function() {
+        this.classList.toggle("is-open");
+
+        let content = this.nextElementSibling;
+        console.log(content);
+
+        if (content.style.maxHeight) {
+            //this is if the accordion is open
+            content.style.maxHeight = null;
+        } else {
+            //if the accordion is currently closed
+            content.style.maxHeight = content.scrollHeight + "px";
+            console.log(content.style.maxHeight);
+        }
+    };
+});
