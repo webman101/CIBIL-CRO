2538d9590c5ab79106222a5e2e07ab25097be8b6
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-09-23 13:55:27 +0530

    questionpopup

---
+++ survey-questions-popup/js/custom.js
@@ -0,0 +1,76 @@
+// $(document).ready(function($) {
+
+//     $("#register-form").validate({
+//         rules: {
+//             name: "required",
+//             number: {
+//                 required: true,
+//                 minlength: 10
+//             },
+//             city: "required",
+//             gender: "required"
+
+//         },
+//         messages: {
+//             name: "Please enter your Name",
+//             number: {
+//                 required: "Please enter your Number",
+//                 minlength: "Your Number must be at 10 characters long"
+//             },
+//             city: "Please enter your city",
+//             gender: "This field is required"
+//         },
+//         errorPlacement: function(error, element) {
+//             if (element.is(":radio")) {
+//                 error.appendTo(element.parents('.form-group'));
+//             } else { // This is the default behavior 
+//                 error.insertAfter(element);
+//             }
+//         },
+//         submitHandler: function(form) {
+//             form.submit();
+//         }
+
+//     });
+// });
+
+
+
+// const mouseEvent = e => {
+//     if (!e.toElement && !e.relatedTarget && e.clientY < 10) {
+//         document.removeEventListener('mouseout', mouseEvent);
+
+//         document.querySelector('.exit-intent-popup').classList.add('visible');
+//     }
+// };
+
+// document.addEventListener('mouseout', mouseEvent);
+
+
+// const exit = e => {
+//     if (e.target.className === 'close-tab') {
+//         document.querySelector('.exit-intent-popup').classList.remove('visible');
+//     }
+// };
+
+// document.querySelector('.exit-intent-popup').addEventListener('click', exit);
+
+
+// // $('#popup-name').on('change', function() {
+// //     if ($(this).val() !== "") {
+      
+// //     }
+// // });
+// // if($("#html").prop('checked') == true){
+// //     $('#submit').removeClass('disabled');
+// // }
+
+//  $('#html').on('change', function() {
+//     if ($("#html").is(":checked")) {
+//         $('#submit').removeClass('disabled');
+//     }
+//     else{
+//         $('#submit').addClass('disabled');
+
+//     }
+//  });
