c146df528de796495e7883b185ea2e9d7579abe0
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-11-12 12:25:33 +0530

    score simulator page

---
+++ alerts-landing-page/lp.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.base-plan').click(function(e) {
+        e.preventDefault();
+        $('.base-plan').removeClass('border-right-0');
+
+    });
+    $('.base-standard').click(function(e) {
+        e.preventDefault();
+        $('.base-plan').addClass('border-right-0');
+        $('.base-standard').removeClass('border-right-0');
+
+    });
+    $('.base-premium').click(function(e) {
+        e.preventDefault();
+        $('.base-plan').removeClass('border-right-0');
+        $('.base-standard').addClass('border-right-0');
+    });
+});
