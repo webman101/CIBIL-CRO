8fba53b0aae81202968effcbf4b3e334f6363f77
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-11-15 15:06:41 +0530

    448

--- alerts-landing-page/lp.js
+++ alerts-landing-page/lp.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('.base-plan').click(function(e) {
         e.preventDefault();
         $('.base-plan').removeClass('border-right-0');
+        $('.base-standard').removeClass('border-right-0');
+
 
     });
     $('.base-standard').click(function(e) {
