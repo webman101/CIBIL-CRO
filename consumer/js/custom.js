5d2318c3a2f012576bd81363a0cc103b08c1fcd4
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-11-22 11:43:23 +0530

    signup text

---
+++ consumer/js/custom.js
@@ -0,0 +1,91 @@
+$(function() {
+
+    var activeIndex = $('.active-tab').index(),
+        $contentlis = $('.tabs-content li'),
+        $tabslis = $('.tabs li');
+
+    // Show content of active tab on loads
+    $contentlis.eq(activeIndex).show();
+
+    $('.tabs').on('click', 'li', function(e) {
+        var $current = $(e.currentTarget),
+            index = $current.index();
+
+        $tabslis.removeClass('active-tab');
+        $current.addClass('active-tab');
+        $contentlis.hide().eq(index).show();
+    });
+});
+
+
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+    accordion.onclick = function() {
+        this.classList.toggle("is-open");
+
+        let content = this.nextElementSibling;
+        console.log(content);
+
+        if (content.style.maxHeight) {
+            //this is if the accordion is open
+            content.style.maxHeight = null;
+        } else {
+            //if the accordion is currently closed
+            content.style.maxHeight = content.scrollHeight + "px";
+            console.log(content.style.maxHeight);
+        }
+    };
+});
+
+$('.banner-slider').slick({
+    arrows: false,
+    dots: true,
+    center: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+
+});
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+    /* the viewport is less than 768 pixels wide */
+    $('.mobile-slider').slick({
+        arrows: false,
+        dots: true,
+        center: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+
+    });
+}
+if (window.matchMedia("(max-width: 768px)").matches) {
+    /* the viewport is less than 768 pixels wide */
+    $('.mobile-slider-2').slick({
+        arrows: false,
+        dots: true,
+        center: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+
+    });
+}
+$('.info').mouseenter(function() {
+    $('.tool-tip').addClass('opacity-1');
+    $('.info-img').addClass('opacity-1');
+
+});
+$('.info').mouseleave(function() {
+    $('.tool-tip').removeClass('opacity-1');
+    $('.info-img').removeClass('opacity-1');
+
+});
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+    /* the viewport is less than 768 pixels wide */
+    $("document").ready(function() {
+        $(".is-open").ready(function() {
+            $(".is-open").click();
+        });
+    });
+
+}
