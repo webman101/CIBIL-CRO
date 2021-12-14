8f0249f0da8c922221fa7e3545fe5492ed6b3e10
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-12-14 14:52:32 +0530

    footer

--- cibil-dashboard/js/custom.js
+++ cibil-dashboard/js/custom.js
@@ -25,3 +25,22 @@ $('.for-overlay .closeovelay').click(function(e) {
     e.preventDefault();
     $(this).closest('.for-overlay').children('.overlay-card').addClass('d-none');
 });
+
+
+$(document).ready(function() {
+    // Show the first tab and hide the rest
+    $('#largetabs-nav li:first-child').addClass('active');
+    $('.tab-content').hide();
+    $('.tab-content:first').show();
+
+    // Click function
+    $('#largetabs-nav li').click(function() {
+        $('#largetabs-nav li').removeClass('active');
+        $(this).addClass('active');
+        $('.tab-content').hide();
+
+        var activeTab = $(this).find('a').attr('href');
+        $(activeTab).fadeIn();
+        return false;
+    });
+});
