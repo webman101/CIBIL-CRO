175d76eac60682c3711b653293071d34cc92b4b9
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2022-01-10 15:17:51 +0530

    ui

--- cibil-dashboard/js/custom.js
+++ cibil-dashboard/js/custom.js
@@ -362,3 +362,43 @@ function toggleItem() {
 }
 
 
+
+
+var tab; // заголовок вкладки
+var tabContent; // блок содержащий контент вкладки
+
+
+window.onload=function() {
+    tabContent=document.getElementsByClassName('reporttabContent');
+    tab=document.getElementsByClassName('reporttab');
+    hideTabsContent(1);
+}
+
+document.getElementById('reporttabs').onclick= function (event) {
+    var target=event.target;
+    if (target.className=='reporttab') {
+       for (var i=0; i<tab.length; i++) {
+           if (target == tab[i]) {
+               showTabsContent(i);
+               break;
+           }
+       }
+    }
+}
+
+function hideTabsContent(a) {
+    for (var i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+        tab[i].classList.remove('whiteborder');
+    }
+}
+
+function showTabsContent(b){
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('whiteborder');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
