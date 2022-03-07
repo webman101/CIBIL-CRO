f27515858726b86ee5d272397e8f7fe23be0e44e
Siddesh <siddesh@ajency.in>
2022-03-07 13:33:49 +0530

    Watch and learn section

--- cibil-beworthy/assets/js/slickJs.js
+++ cibil-beworthy/assets/js/slickJs.js
@@ -52,65 +52,82 @@ $(document).ready(function () {
         $('.article-slider-js').slick('slickNext')
     })
 
-    $('#process-slick').slick({
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    centerMode: true,
-                    centerPadding: '50px',
-                }
-            }
-        ]
+    $('.video-list-item').click(function(){
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active')
+        let videoId = $(this).attr('data-toggle')
+        console.log(videoId)
+        $('.video-wrap').hide().removeClass('play-video')
+        $('.video-wrap iframe').removeAttr('src')
+        $('#video'+videoId).show()
     })
 
-    $('#icon-slick').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false,
-        variableWidth: true,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    centerMode: true,
-                    centerPadding: '20px',
-                }
-            }
-        ]
+    $('.video-wrap').click(function(){
+        $(this).addClass('play-video')
+        videoSrc = $(this).find('iframe').attr('data-src')
+        console.log("src",videoSrc)
+        $(this).find('iframe').attr('src', videoSrc);
     })
 
-    let bannerGif = $('.banner-gif').attr('lsrc');
-    $('.banner-gif').attr('src',bannerGif);
-    $('.banner-gif').show()
+    // $('#process-slick').slick({
+    //     infinite: false,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     dots: false,
+    //     responsive: [
+    //         {
+    //             breakpoint: 992,
+    //             settings: {
+    //                 slidesToShow: 2
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 centerMode: true,
+    //                 centerPadding: '50px',
+    //             }
+    //         }
+    //     ]
+    // })
+
+    // $('#icon-slick').slick({
+    //     infinite: true,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     dots: false,
+    //     variableWidth: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 992,
+    //             settings: {
+    //                 slidesToShow: 2
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 centerMode: true,
+    //                 centerPadding: '20px',
+    //             }
+    //         }
+    //     ]
+    // })
+
+    // let bannerGif = $('.banner-gif').attr('lsrc');
+    // $('.banner-gif').attr('src',bannerGif);
+    // $('.banner-gif').show()
 });
 
-$(window).on("load resize",function(e){
-    var bgHeight = $(".hero-section.new .right-side .main-title").innerHeight();
-    $(".hero-section.new .right-side .gif-container .bg-element").css("height",bgHeight);
+// $(window).on("load resize",function(e){
+//     var bgHeight = $(".hero-section.new .right-side .main-title").innerHeight();
+//     $(".hero-section.new .right-side .gif-container .bg-element").css("height",bgHeight);
 
-    var videoSecHeight = $(".video-section .video-section-wrapper").innerHeight();
-    var videoContainerTop = (videoSecHeight - 419)/2;
-    $(".video-section .video-section-wrapper .video-container").css("top",videoContainerTop);
-});
+//     var videoSecHeight = $(".video-section .video-section-wrapper").innerHeight();
+//     var videoContainerTop = (videoSecHeight - 419)/2;
+//     $(".video-section .video-section-wrapper .video-container").css("top",videoContainerTop);
+// });
