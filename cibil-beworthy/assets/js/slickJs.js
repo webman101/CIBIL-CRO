8a1d8843dbc46d92da1f8986f70076b47b2e35c0
Siddesh <siddesh@ajency.in>
2022-03-07 20:03:51 +0530

    Quiz backend

--- cibil-beworthy/assets/js/slickJs.js
+++ cibil-beworthy/assets/js/slickJs.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
         $(this).siblings().removeClass('active');
         $(this).addClass('active')
         let videoId = $(this).attr('data-toggle')
-        console.log(videoId)
+        // console.log(videoId)
         $('.video-wrap').hide().removeClass('play-video')
         $('.video-wrap iframe').removeAttr('src')
         $('#video'+videoId).show()
@@ -65,69 +65,81 @@ $(document).ready(function () {
     $('.video-wrap').click(function(){
         $(this).addClass('play-video')
         videoSrc = $(this).find('iframe').attr('data-src')
-        console.log("src",videoSrc)
+        // console.log("src",videoSrc)
         $(this).find('iframe').attr('src', videoSrc);
     })
 
-    // $('#process-slick').slick({
-    //     infinite: false,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     dots: false,
-    //     responsive: [
-    //         {
-    //             breakpoint: 992,
-    //             settings: {
-    //                 slidesToShow: 2
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 centerMode: true,
-    //                 centerPadding: '50px',
-    //             }
-    //         }
-    //     ]
-    // })
+    // quiz
 
-    // $('#icon-slick').slick({
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     dots: false,
-    //     variableWidth: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 992,
-    //             settings: {
-    //                 slidesToShow: 2
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 centerMode: true,
-    //                 centerPadding: '20px',
-    //             }
-    //         }
-    //     ]
-    // })
+    const valueJSON = {
+        '1' : 'c',
+        '2' : 'd',
+        '3' : 'b'
+    }
 
-    // let bannerGif = $('.banner-gif').attr('lsrc');
-    // $('.banner-gif').attr('src',bannerGif);
-    // $('.banner-gif').show()
+    function randomSort(a, b) {  
+        return 0.5 - Math.random();
+    }
+    var numberArray = [1,2,3];
+    var randomOrder = numberArray.sort(randomSort);
+    // console.log(randomOrder)
+    var counterArray = 0;
+    var correctCount = 0;
+
+    $('.strat-quiz-btn').click(function(e){
+        e.preventDefault();
+        $('.start-card').hide()
+        let progress = ((counterArray+1)/(numberArray.length))*100;
+        $('.quiz-progress .bar').css('width',progress+'%')
+        let questionText = 'Question '+(counterArray+1)+'/'+numberArray.length;
+        $('[data-card='+randomOrder[counterArray]+'] .question-number').text(questionText)
+        $('[data-card='+randomOrder[counterArray]+']').show()
+        counterArray = counterArray+1 ;
+    })
+
+    $('.proceed-next').click(function(e){
+        e.preventDefault();
+        $('.quiz-card').hide()
+        if(counterArray == (numberArray.length)){
+            let result = (correctCount/counterArray)*100;
+            if(result <= 20){
+                $('#novice').show()
+            }
+            else if(result <= 50){
+                $('#scholar').show()
+            }
+            else if( result <= 90){
+                $('#rockstar').show()
+            }
+            else{
+                $('#master').show()
+            }
+        }else{
+            let progress = ((counterArray+1)/(numberArray.length))*100;
+            $('.quiz-progress .bar').css('width',progress+'%')
+            let questionText = 'Question '+(counterArray+1)+'/'+numberArray.length;
+            $('[data-card='+randomOrder[counterArray]+'] .question-number').text(questionText)
+            $('[data-card='+randomOrder[counterArray]+']').show()
+            counterArray = counterArray+1;
+        }
+    })
+
+    $('.option').click(function(){
+        $(this).siblings().css('pointer-events','none')
+        let parentCard = $(this).closest('.quiz-card');
+        let questionNumber = parentCard.attr('data-card')
+        let optionNumber = $(this).attr('data-value');
+        if(valueJSON[questionNumber] == optionNumber){
+            correctCount = correctCount+1;
+            $(this).addClass('correct')
+        }
+        else{
+            $(this).siblings('[data-value='+valueJSON[questionNumber]+']').addClass('correct')
+            $(this).addClass('wrong')
+        }
+        // console.log(valueJSON[questionNumber])
+        parentCard.find('.proceed-next').removeClass('disabled');
+        parentCard.find('.card-details').show()
+    })
+
 });
-
-// $(window).on("load resize",function(e){
-//     var bgHeight = $(".hero-section.new .right-side .main-title").innerHeight();
-//     $(".hero-section.new .right-side .gif-container .bg-element").css("height",bgHeight);
-
-//     var videoSecHeight = $(".video-section .video-section-wrapper").innerHeight();
-//     var videoContainerTop = (videoSecHeight - 419)/2;
-//     $(".video-section .video-section-wrapper .video-container").css("top",videoContainerTop);
-// });
