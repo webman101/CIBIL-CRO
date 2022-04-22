b571aa3b229b0112314d33e42a2681b65a75603e
Mamata <mamata@ajency.in>
2022-04-22 19:47:13 +0530

    table functionality

--- Member-contact-list/member-search.js
+++ Member-contact-list/member-search.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
        $(this).parents().eq(1).addClass('focused');
      });
 
-     var banksApi = "banks.json";
+
+/* Search */
+var banksApi = "banks.json";
 $.getJSON(banksApi, function(data){
     /* console.log(data); */
 
@@ -49,4 +51,35 @@ $.getJSON(banksApi, function(data){
         source: substringMatcher(banks)
       });
 });
+
+/* Table */
+  $('#members-table').DataTable( {
+      "ajax": 'members.json'
+  } );
+
+ $('#search-input').keyup(function() {
+    $('.dataTables_filter input[type=search]').keyup();
+    $('.dataTables_filter input[type=search]').val($(this).val());
+});
+
+/* hide */
+$('.members-table-section').css("display", "none");
+$('.members-table-section .hide-column').css("display", "none");
+$('.members-table-section .dataTables_wrapper .row:first-child').css("display", "none");
+$('.members-table-section .dataTables_wrapper .row:last-child').css("display", "none");
+$('.members-table-section #members-table tbody tr td:last-child').css("display", "none");
+$('#search-input').keypress(function(){
+  $('.default-section').hide();
+  $('.members-table-section').show();
+  $('.members-table-section #members-table tbody tr td:last-child').css("display", "none");
+});
+
+$('#search-input').keyup(function(){
+  if($(this).val() == ""){
+    $('.members-table-section').hide();
+    $('.default-section').show();
+    $('.members-table-section #members-table tbody tr td:last-child').css("display", "none");
+  }
+});
+
 });
