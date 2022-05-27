6888aa6c9da98cb6a3510db2e951e6ce62364f37
Mamata <mamata@ajency.in>
2022-05-27 17:10:10 +0530

    logout page CR - blog with youtube link

---
+++ logout-page/logout-page.js
@@ -0,0 +1,82 @@
+    /* Slider start */
+    $(".myths-n-facts-slider").slick({
+        dots: true,
+        arrows:false,
+        speed: 500,
+        responsive: [
+            {
+            breakpoint: 768,
+                settings: {
+                    dots:false
+                }
+            }
+        ]
+    });
+    /* slider end */
+
+/* copy link */
+  var copyButton = document.querySelector('#clipboard');
+  var copyInput = document.querySelector('.copy');
+  copyButton.addEventListener('click', function(e) {
+    e.preventDefault();
+	let copiedLink = copyInput.value;
+    var text = copyInput.select();
+    document.execCommand('copy');
+	copyButton.classList.add("copied");
+ 	setTimeout(function(){
+            copyButton.classList.remove("copied");
+    }, 1000);
+  });
+
+  copyInput.addEventListener('click', function() {
+    this.select();
+  });
+  /* copy link end */
+
+
+/* youtube player */
+
+  $(".youtube-link").click(function(e){
+      e.preventDefault();
+      let youtubeLink = $(this).attr("href");
+      console.log(youtubeLink);
+      $("#youtube-video").attr("src",youtubeLink);
+      $(".modal-popup").toggleClass("opened");
+  });
+  $(".close-btn").click(function(){
+    $(".modal-popup").toggleClass("opened");
+  });
+
+
+  // global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+    player = new YT.Player('youtube-video', {
+        events: {
+            // call this function when player is ready to use
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+
+    // bind events
+    $(".close-btn").click(function(){
+        player.stopVideo();
+    });
+    $(".youtube-link").click(function(){
+        player.playVideo();
+    });
+}
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+/* youtube player end */
