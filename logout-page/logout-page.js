664ac9d8b7b21c3190a8cf391355900b31cecf16
Mamata <mamata@ajency.in>
2022-05-31 17:15:32 +0530

    Logout page changes

--- logout-page/logout-page.js
+++ logout-page/logout-page.js
@@ -36,7 +36,7 @@
 
 /* youtube player */
 
-  $(".youtube-link").click(function(e){
+/*   $(".youtube-link").click(function(e){
       e.preventDefault();
       let youtubeLink = $(this).attr("href");
       $("#youtube-video").attr("src",youtubeLink);
@@ -45,17 +45,11 @@
   $(".close-btn").click(function(){
     $(".modal-popup").toggleClass("opened");
   });
-
-
-  // global variable for the player
 var player;
 
-// this function gets called when API is ready to use
 function onYouTubePlayerAPIReady() {
-    // create the global player from the specific iframe (#video)
     player = new YT.Player('youtube-video', {
         events: {
-            // call this function when player is ready to use
             'onReady': onPlayerReady
         }
     });
@@ -63,7 +57,6 @@ function onYouTubePlayerAPIReady() {
 
 function onPlayerReady(event) {
 
-    // bind events
     $(".close-btn").click(function(){
         player.stopVideo();
     });
@@ -72,10 +65,9 @@ function onPlayerReady(event) {
     });
 }
 
-// Inject YouTube API script
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); */
 
 /* youtube player end */
