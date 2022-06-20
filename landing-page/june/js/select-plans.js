7e58fd866cbb83d2059f7a8b809415fb89cb707d
Mamata <mamata@ajency.in>
2022-06-20 18:15:26 +0530

    mobile responsiveness - banner section, plans section & slider section

---
+++ landing-page/june/js/select-plans.js
@@ -0,0 +1,9 @@
+if ($(window).width() < 992) {
+    $('.report-slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+}
