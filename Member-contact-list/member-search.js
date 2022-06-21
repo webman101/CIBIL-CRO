fe9ca386f1fb8cbfcc60af103017f2038f07e350
Mamata <mamata@ajency.in>
2022-06-21 16:44:42 +0530

    Nodal Officer List Page - Merged json files & js chnages

--- Member-contact-list/member-search.js
+++ Member-contact-list/member-search.js
@@ -7,30 +7,44 @@ $(document).ready(function(){
 
 
 /* Search */
-var banksApi = "banks.json";
+var banksApi = "bank-members.json"; // add your json file path
+
+const unique = (value, index, self) => {
+  return self.indexOf(value) === index
+}
+
 $.getJSON(banksApi, function(data){
     var banks = data;
-    /* console.log(banks); */
+    /* console.log("banks: ", banks); */
 
     var substringMatcher = function(strs) {
     return function findMatches(q, cb) {
       var matches, substringRegex;
+      /* console.log(cb); */
 
       // an array that will be populated with substring matches
       matches = [];
 
       // regex used to determine if a string contains the substring `q`
       substrRegex = new RegExp(q, 'i');
-
+      /* console.log(q); */
       // iterate through the pool of strings and for any string that
       // contains the substring `q`, add it to the `matches` array
-      $.each(strs, function(i, str) {
-        if (substrRegex.test(str.value)) {
-          matches.push(str);
+
+      $.each(strs.data, function(i, str) {
+        /* console.log("str 5 :", str[5]); */
+        if (substrRegex.test(str[5])) {
+          matches.push(str[5]);
         }
       });
 
-      cb(matches);
+      var matches1 = matches.filter(unique).map(e =>{
+        return ({value: e})
+      });
+
+      console.log(matches1);
+
+      cb(matches1);
     };
 };
 $('.search-bar .typeahead').typeahead({
@@ -53,7 +67,7 @@ $('.search-bar .typeahead').typeahead({
 
 /* Table */
   $('#members-table').DataTable( {
-      "ajax": 'members.json',
+      "ajax": 'bank-members.json', // add your json file path
       "columns": [
         { "searchable": false },
         { "searchable": false },
@@ -64,9 +78,30 @@ $('.search-bar .typeahead').typeahead({
       ]
   } );
 
+function defaultState(){
+  $('.members-table-section').hide();
+  $('.error-section').hide();
+  $('.default-section').show();
+}
+
 function emptyField(){
   $('#search-input').val("");
   $('.dataTables_filter input[type=search]').val("");
+}
+
+function noMemberData(){
+  var noMenmberData = $("#members-table tbody tr:first-child td:first-child");
+  if (noMenmberData.html()){
+    ShowResult();
+    closeKeyboard();
+    /* console.log("table has data!"); */
+  }
+  else{
+    $('.members-table-section').hide();
+    $('.default-section').hide();
+    $('.error-section').show();
+   /*  console.log("No data!"); */
+  }
 }
 
 function ShowResult(){
@@ -106,14 +141,13 @@ $('#search-input').keyup(function() {
 });
 
 $('.typeahead').on('typeahead:selected', function(evt, item) {
-  /* console.log(item.bank_name); */
-  $('.dataTables_filter input[type=search]').val(item.bank_name);
+  $('.dataTables_filter input[type=search]').val(item.value);
   $('.dataTables_filter input[type=search]').keyup();
-  ShowResult();
-  closeKeyboard();
+  noMemberData();
 });
 
 $('#search-input').on('keypress',function(e) {
+  defaultState();
   if(e.which == 13) {
     ShowResult();
     $('.members-table-section').hide();
