54d828258c7f8055910c356854b69b4220792f1c
Mamata <mamata@ajency.in>
2022-06-28 12:37:17 +0530

    Login page - styling & responsiveness

--- login-flow/js/login.js
+++ login-flow/js/login.js
@@ -1,3 +1,4 @@
+/* identify if user is existing or new */
 localStorage.setItem("loggedInUser", "True");
 const user = localStorage.getItem("loggedInUser");
 if (user) {
@@ -5,3 +6,19 @@ if (user) {
 } else {
   console.log("Login");
 }
+
+/* container */
+$(window).on("load resize", function (e) {
+  let HeaderContainer = $(".header-container").width();
+  let formSection = $(".login-form-section").outerWidth();
+
+  let totalMargin = (formSection - HeaderContainer) / 2;
+
+  let fullRightPanel = $(".login-form-section .right-panel");
+  fullRightPanel.css("padding-right", totalMargin);
+
+  let footerText = $(".footer-text");
+  let footerAlign = (fullRightPanel.width()/2) - (footerText.width()/2) ;
+  console.log(footerAlign);
+  footerText.css("left", footerAlign);
+});
