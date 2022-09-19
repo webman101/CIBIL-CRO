3e29a7e5986739a3055ef4b990687ed200432b43
Siddesh <siddesh@ajency.in>
2022-09-19 14:29:55 +0530

    Added credential: include to request header

--- landing-page/july/resources/js/newsletter-form.js
+++ landing-page/july/resources/js/newsletter-form.js
@@ -57,7 +57,8 @@ async function saveData() {
       method: "GET",
       headers: {
           "Content-Type": "application/x-www-form-urlencoded"
-      }
+      },
+      credentials: "include"
   }
   let response = await fetch("https://atlasls-za-test.sd.demo.truelink.com/CreditView/mobile/entry1_0.page?tl.partner=CIBILMKT", config)
   let data = await response.json();
@@ -74,9 +75,9 @@ async function saveData() {
       body: `tl.email-address=${email.value}${input_name}&requestToken=${data.ud.ResponseDetails.csrfToken}&visitToken=${data.ud.ResponseDetails.visitToken}&DestinationPage=mobile/cibil_marketing_redirect`,
       headers: {
           "Content-Type": "application/x-www-form-urlencoded"
-      }
+      },
+      credentials: 'include'
   }
-  // credentials: 'include'
   console.log(config2)
   // response = await fetch("https://atlasls-za-test.sd.demo.truelink.com/CreditView/mobile/redirect1_0.page?action=MARKETING_CUSTOMER&tl.partner=CIBILMKT", config2)
   response = await fetch("https://atlasls-za-test.sd.demo.truelink.com/CreditView/mobile/cibil_marketing_redirect.page?action=MARKETING_CUSTOMER&tl.partner=CIBILMKT", config2)
