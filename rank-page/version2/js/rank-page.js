7e47fd7d96e20477e0b4339578a51dfd1f427c18
Mamata <mamata@ajency.in>
2022-10-05 16:10:55 +0530

    Rank page - FAQ section

--- rank-page/version2/js/rank-page.js
+++ rank-page/version2/js/rank-page.js
@@ -1,5 +1,14 @@
+//banner slider
 $('.banner-slider').slick({
     arrows: false,
     dots: true,
     appendDots: '.cibil-slick-dots'
 });
+
+//faq
+$('.panel-collapse').on('show.bs.collapse', function () {
+    console.log($(this).prev().addClass("show"));
+  });
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    console.log($(this).prev().removeClass("show"));
+  });
