8ea193bf6658a58e60eac4fde8544846b7a42b33
Mamata <mamata@ajency.in>
2022-10-13 12:51:23 +0530

    rank page - js changes for the UAT

--- rank-page/version2/js/rank-page.js
+++ rank-page/version2/js/rank-page.js
@@ -1,14 +1,16 @@
 //banner slider
-$('.banner-slider').slick({
-    arrows: false,
-    dots: true,
-    appendDots: '.cibil-slick-dots'
+$(".banner-slider").slick({
+  arrows: false,
+  dots: true,
+  appendDots: ".cibil-slick-dots",
 });
 
 //faq
-$('.panel-collapse').on('show.bs.collapse', function () {
-    $(this).prev().addClass("show");
-  });
-  $('.panel-collapse').on('hide.bs.collapse', function () {
-    $(this).prev().removeClass("show");
-  });
+$(".panel-collapse").on("show.bs.collapse", function () {
+  $(this).prev().addClass("show");
+  $(this).addClass("show");
+});
+$(".panel-collapse").on("hide.bs.collapse", function () {
+  $(this).prev().removeClass("show");
+  $(this).removeClass("show");
+});
