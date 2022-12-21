0f76ea291ec59d93473ba31be10cfab70a2df0bb
Siddesh <siddesh@ajency.in>
2022-12-21 14:57:41 +0530

    Loan Tabs UI- Mobile + Desktop

--- loan-offers/script.js
+++ loan-offers/script.js
@@ -12,3 +12,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     })
 });
+
+function customSelectInput(element){
+    $('.custom-select-input').not($(element)).removeClass('opened');
+    $('.custom-select-input').not($(element)).find('.custom-select-options').hide('fast');
+    $(element).toggleClass('opened')
+    $(element).find('.custom-select-options').toggle('fast');
+}
+
+function customSelectTab(element){
+    $(element).siblings().removeClass("selected")
+    $(element).addClass('selected')
+    $(element).parents('.custom-select-input').find('.custom-select-value').html($(element).html())
+    $('.tab-panel').removeClass('active');
+    $($(element).data('target')).addClass('active');
+    $('.tab').removeClass('active');
+    $('.tab[data-target="'+$(element).data('target')+'"]').addClass('active');
+}
+
+function selectTab(element){
+    // $(element).siblings().removeClass("active")
+    // $(element).addClass('active')
+    // $('.tab-panel').removeClass('active');
+    // $($(element).data('target')).addClass('active');
+    // $('.tab').removeClass('active');
+    // $('.tab[data-target="'+$(element).data('target')+'"]').addClass('active');
+    $('.custom-select-options>span[data-target="'+$(element).data('target')+'"]').click()
+}
